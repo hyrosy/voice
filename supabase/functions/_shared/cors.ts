877c8608949fd1c155a85dd2f3095acfa9f56c3d
hyrosy <support@hyrosy.com>
2025-10-29 15:05:09 +0100

    Update cors.ts

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,4 +1,4 @@
 export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*', // Use '*' for development, restrict in production
+  'Access-Control-Allow-Origin': 'https://www.ucpmaroc.com/', // Use '*' for development, restrict in production
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 }
